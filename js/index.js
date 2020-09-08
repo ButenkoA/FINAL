6469d3ee477b19444252438bc5604f6ad7fb8aca
ButenkoA <69208413+ButenkoA@users.noreply.github.com>
2020-09-08 17:22:38 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-document.querySelector('.login-form input[type=submit]')
-    .addEventListener('click', login);
-function login(e) {
-    e.preventDefault();
-    fetch('login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify({
-            name: document.querySelector('.login-form input[name=name]').value,
-            email: document.querySelector('.login-form input[name=email]').value,
-            tel: document.querySelector('.login-form input[name=tel]').value
-        })
-    })
-    
+function hideCollapseMenu() {
+  document.getElementById("navbarNav").classList.remove("show");
+}
 
-// document.getElementsByClassName(:nav-link).addEventListener('click', hideCollapseMenu);
+// document.querySelector('.login-form input[type=submit]')
+//     .addEventListener('click', login);
+// function login(e) {
+//     e.preventDefault();
+//     fetch('login', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             // 'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//         body: JSON.stringify({
+//             name: document.querySelector('.login-form input[name=name]').value,
+//             email: document.querySelector('.login-form input[name=email]').value,
+//             tel: document.querySelector('.login-form input[name=tel]').value
+//         })
+//     })
+// }
 
-//   function hideCollapseMenu(){
-//       document.getElementsByClassName("navbarNav").classList.remove("show")
-//   }
+// document.getElementsByClassName('nav-link').addEventListener('click', hideCollapseMenu);
 
 //   $('.navbar-nav>li>a').on('click', function(){
 //     $('.navbar-collapse').collapse('hide');
